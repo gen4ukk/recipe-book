fa320f056b728f0b798e4e862a2298a0b540c9b5
gen4ukk <gen4ukk@gmail.com>
2022-12-02 22:18:28 +0200

    chapter 12  last part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipeBookRoutes } from './recipe-book.routing';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
@@ -25,7 +26,8 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
       RecipeItemComponent,
       ShoppingEditComponent,
       DropdownDirective,
-      RecipeStartComponent
+      RecipeStartComponent,
+      RecipeEditComponent
    ],
   imports: [
     BrowserModule,
